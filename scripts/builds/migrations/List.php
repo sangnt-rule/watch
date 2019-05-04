bc7d5d452e3c2ab4cf4af14343882c7fc8b4bb6d
ks <anh.trandothe@savemoney.vn>
2019-05-04 19:16:44 +0700

    save

--- scripts/builds/migrations/List.php
+++ scripts/builds/migrations/List.php
@@ -15,6 +15,11 @@ class MigrationList
             '2019/04/24/02.category.sql',
             '2019/04/24/03.watch.sql',
             '2019/04/28/01.add_column_machine.sql',
+            '2019/05/04/01.create_table_cord.sql',
+            '2019/05/04/02.change_column_watch.sql',
+            '2019/05/04/03.insert_data_category.sql',
+            '2019/05/04/05.create_table_banner.sql',
+            '2019/05/04/06.add_column_watch.sql',
         );
     }
 }
---
+++ scripts/builds/migrations/scripts/2019/05/04/06.add_column_watch.sql
@@ -0,0 +1,2 @@
+alter table watch
+  add column watch_description text;
