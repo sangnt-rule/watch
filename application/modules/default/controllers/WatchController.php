fc1e22ac6313369422f9c227a6c28d04ede5f3df
sang <sangnguyen>
2019-05-10 20:10:53 +0700

    save

--- application/modules/default/controllers/WatchController.php
+++ application/modules/default/controllers/WatchController.php
@@ -5,13 +5,9 @@ class WatchController extends Application_Controller_FrontEnd
     {
         $idMachine = $this->getRequest()->getParam('machine');
         $idCord = $this->getRequest()->getParam('cord');
-        $data = Model_Watch::getInstance()->getByFkMachineFkCord($idMachine, $idCord);
+        $search = $this->getRequest()->getParam('search');
+        $data = Model_Watch::getInstance()->search($idMachine, $idCord, $search);
         $this->view->assign('watch', $data);
-
-    }
-    public function search()
-    {
-
 
     }
     public function detailAction()
